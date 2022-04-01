8f34aa0fefbc2ed1ab2bdbc6d65cecca2b99534e
TimofeyTvorogov <mr.tvorogov2004@mail.ru>
2022-04-01 20:33:53 +0800

    bg+minor improvement

--- Gunslinger/app/src/main/java/com/example/gunslinger/DrawMap.java
+++ Gunslinger/app/src/main/java/com/example/gunslinger/DrawMap.java
@@ -7,19 +7,26 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
 
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class DrawMap extends View {
 Resources resources;
-Bitmap cobbleStone;
-Bitmap brick;
-Bitmap water;
+Bitmap cobbleStone, brick, water;
+//ArrayList<Bitmap> textureArrayList;
+int textureWidth, textureHeight;
+
+
+
 
 
 
@@ -29,7 +36,10 @@ public DrawMap(Resources resources, Context context) {
     cobbleStone = BitmapFactory.decodeResource(resources, R.drawable.block);
     brick = BitmapFactory.decodeResource(resources, R.drawable.brick);
     water = BitmapFactory.decodeResource(resources, R.drawable.water);
+    textureHeight = cobbleStone.getHeight();
+    textureWidth = cobbleStone.getWidth();
     //TODO затолкать в список битмапов
+
 
 }
 //генерирует строковый массив из файла
@@ -68,26 +78,31 @@ public DrawMap(Resources resources, Context context) {
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+        //рисуем задний фон
+        canvas.drawColor(Color.DKGRAY);
+
         //забираем массив из функции
         String [][] mapArr = generateMapArray();
 
         for (int y = 0; y < mapArr.length; y++) {
             for (int x = 0; x < mapArr[y].length; x++) {
                if (mapArr[y][x].equals("t")) {
-                   canvas.drawBitmap(cobbleStone, x*cobbleStone.getWidth(), y*cobbleStone.getHeight(),null);
+                   canvas.drawBitmap(cobbleStone, x*textureWidth, y*textureHeight,null);
                }
                if (mapArr[y][x].equals("b")) {
-                   canvas.drawBitmap(brick, x*brick.getWidth(), y*brick.getHeight(),null);
+                   canvas.drawBitmap(brick, x*textureWidth, y*textureHeight,null);
                }
                if (mapArr[y][x].equals("w")) {
-                   canvas.drawBitmap(water, x*water.getWidth(), y*water.getHeight(),null);
+                   canvas.drawBitmap(water, x*textureWidth, y*textureHeight,null);
                }
 
-// тут хз чё делать, оно кроме свитч кейс ломает и цикл               switch (mapArr[y][x]){
-//                   case "t" : canvas.drawBitmap(cobbleStone,x*cobbleStone.getWidth(),y*getHeight(), null);break;
-//                   case "b" : canvas.drawBitmap(brick,x*getWidth(),y*getHeight(), null);break
-//                   case "w" : canvas.drawBitmap(water,x*getWidth(),y*getHeight(), null);break;
-//              }
+
+// тут хз чё делать, оно кроме свитч кейс ломает и цикл
+//                  switch (mapArr[y][x]){
+//                  case "t" : canvas.drawBitmap(cobbleStone,x*cobbleStone.getWidth(),y*getHeight(), null);break;
+//                  case "b" : canvas.drawBitmap(brick,x*getWidth(),y*getHeight(), null);break;
+//                  case "w" : canvas.drawBitmap(water,x*getWidth(),y*getHeight(), null);break;
+//             }
 
 
             }
